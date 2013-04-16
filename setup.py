6b7be003c4228bd83a6ce62a60d33498f1bebdda
Victor Vasiliev <vasilvv@mit.edu>
2013-04-16 16:45:50 +0000

    In athinfo:   * Use Python distutils instead of autoconf   * Use debhelper7   * Use source format 3.0 (quilt)

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='athinfo',
+      version='10.1',
+      description='Retrieve information about Athena workstations',
+      author='Evan Broder',
+      scripts=['athinfo']
+    )

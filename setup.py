a1e6a3cd11330b6bfe22166ff6d20f252dba9a1e
Benjamin Barenblat <bbaren@mit.edu>
2014-02-09 12:27:18 -0800

    Version bump to 10.3
    In preparation for splitting the Debian packaging data into its own
    branch, I’m bumping the version on this package so a rebuild gets
    triggered at the appropriate time.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='athinfo',
-      version='10.1',
+      version='10.3',
       description='Retrieve information about Athena workstations',
       author='Evan Broder',
       scripts=['athinfo']
